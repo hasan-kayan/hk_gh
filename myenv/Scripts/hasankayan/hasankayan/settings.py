cc9e61582535acd33aed9892b9191ec989d7f481
hasan-kayan <hasankayan2000@hotmail.com>
2023-02-25 02:53:22 +0300

    Introduced First Apps

--- myenv/Scripts/hasankayan/hasankayan/settings.py
+++ myenv/Scripts/hasankayan/hasankayan/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'BlogPage',
+    'CV',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
