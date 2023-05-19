96d1d806383b91da2ea06e86f6dd230b3a1dce03
hasan-kayan <hasankayan2000@hotmail.com>
2023-05-19 12:06:50 +0300

    Restarting to hasankayan.net project

--- myenv/Scripts/hasankayan/CV/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CvConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CV'
--- myenv/Scripts/hasankayan/CV/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views # This is the line that was added (the dot means the current directory)
-
-
-urlpatterns = [
-    
-    path("portfolio", views.portfolio),
-   
-]
