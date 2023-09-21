4d035f601065b3a347bec102e1f6f6d868324463
hasan-kayan <hasankayan2000@hotmail.com>
2023-09-21 17:40:30 +0300

    restarted

--- node_modules/@rive-app/react-canvas/README.md
+++
@@ -1,3 +0,0 @@
-# @rive-app/react-canvas
-
-Output for `rive-react` using the backing `@rive-app/canvas` JS runtime
--- node_modules/@rive-app/react-canvas/dist/types/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { UseRiveOptions } from './types';
-export declare function getOptions(opts: Partial<UseRiveOptions>): {
-    useDevicePixelRatio: boolean;
-    fitCanvasToArtboardHeight: boolean;
-    useOffscreenRenderer: boolean;
-    shouldResizeCanvasToContainer: boolean;
-} & Partial<UseRiveOptions>;
