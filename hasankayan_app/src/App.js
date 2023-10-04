178506bdc701ceceb10bf1a7c7f4fc1db11edc75
hasan-kayan <hasankayan2000@hotmail.com>
2023-10-04 23:30:36 +0300

    Structure

--- hasankayan_app/src/App.js
+++ hasankayan_app/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css'; // This line imports your application's CSS file
-import About from './components/navbar/navbar'; // Imports the about component
-import Contact from './components/about/about'; // Imports the contact component
-import Footer from './components/projects/projects'; // Imports the footer component
-import Header from './components/home/home'; // Imports the header component
-import Projects from './components/contact/contact'; // Imports the projects component
+import Navbar from './components/navbar/navbar'; // Imports the about component
+import About from './components/about/about'; // Imports the contact component
+import Projects from './components/projects/projects'; // Imports the footer component
+import Home from './components/home/home'; // Imports the header component
+import Contact from './components/contact/contact'; // Imports the projects component
+import { Nav } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       
+
       
     </div>
   );
