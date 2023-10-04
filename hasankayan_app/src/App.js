c9a32897899081b801f5f77676155df21c07e3eb
hasan-kayan <hasankayan2000@hotmail.com>
2023-10-04 21:35:13 +0300

    Structure

--- hasankayan_app/src/App.js
+++ hasankayan_app/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css'; // This line imports your application's CSS file
-import About from './components/about'; // Imports the about component
-import Contact from './components/contact'; // Imports the contact component
-import Footer from './components/footer'; // Imports the footer component
-import Header from './components/header'; // Imports the header component
-import Projects from './components/projects'; // Imports the projects component
+import About from './components/navbar/navbar'; // Imports the about component
+import Contact from './components/about/about'; // Imports the contact component
+import Footer from './components/projects/projects'; // Imports the footer component
+import Header from './components/home/home'; // Imports the header component
+import Projects from './components/contact/contact'; // Imports the projects component
 
 function App() {
   return (
     <div className="App">
-      <Header /> {/* Using the Header component */}
-      <About /> {/* Using the About component */}
-      <Projects /> {/* Using the Projects component */}
-      <Contact /> {/* Using the Contact component */}
-      <Footer /> {/* Using the Footer component */}
+      
+      
     </div>
   );
 }
