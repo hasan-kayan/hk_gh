2d04d8fb707664bd00a898c90fc7813738065cf9
hasan-kayan <hasankayan2000@hotmail.com>
2023-10-26 19:31:27 +0300

    Framer is comming

---
+++ hasan/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#contact">Contact</a></li>
+          
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
