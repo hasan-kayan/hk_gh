afc178f5b262ee63d6b68bf5ab5815d3df1b445d
hasan-kayan <hasankayan2000@hotmail.com>
2023-10-28 13:20:32 +0300

    clear

--- hasan/src/App.js
+++ hasan/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+// Import other components
+import Home from './components/Home/home';
+import Projects from './components/Projects/projects';
+import About from './components/About/about';
+import Contact from './components/Contact/contact';
+import NotFound from './components/notfound'; // Import the 404 component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/components/Home/home" exact component={Home} />
+        <Route path="/components/Projects/projects" component={Projects} />
+        <Route path="/components/About/about" component={About} />
+        <Route path="/components/Contact/contact" component={Contact} />
+        {/* Add the 404 page as the last Route */}
+        <Route component={NotFound} />
+      </Routes>
+    </Router>
   );
 }
 
--- hasan/src/components/Header/Header.js
+++ hasan/src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // Import the CSS you'll write
 
 function Header() {
   return (
-    <header>
-      <h1>My Portfolio</h1>
-      <nav>
-        <ul>
-          <li><a href="#Home">Home</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#about">About Me</a></li>
-          <li><a href="#contact">Contact</a></li>
-          
+    <header className="header">
+      <h1 className="header-title">My Portfolio</h1>
+      <nav className="header-nav">
+        <ul className="header-nav-list">
+          <li className="header-nav-item"><Link to="/">Home</Link></li>
+          <li className="header-nav-item"><Link to="/projects">Projects</Link></li>
+          <li className="header-nav-item"><Link to="/about">About Me</Link></li>
+          <li className="header-nav-item"><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
