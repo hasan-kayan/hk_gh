99aa77a9f505d62862c96899409cbc077aaec996
hasan-kayan <hasankayan2000@hotmail.com>
2023-12-19 00:41:42 +0300

    DEPLOY

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,33 @@
-# Use an official Node runtime as a parent image
-FROM node:14-alpine
+# Use an official Node.js runtime as a base image
+FROM node:14 AS builder
 
-# Set the working directory to /app
+# Set the working directory in the container
 WORKDIR /app
 
-# Copy package.json and package-lock.json to the working directory
+# Copy package.json and package-lock.json to the container
 COPY package*.json ./
 
-# Install app dependencies
+# Install dependencies
 RUN npm install
 
-# Copy the rest of the application code to the working directory
+# Copy the local code to the container
 COPY . .
 
 # Build the React app
 RUN npm run build
 
-# Expose port 80 to the outside world
-EXPOSE 80
+# Use an official Nginx image as a base image for the production environment
+FROM nginx:alpine
 
-# Command to run the application
-CMD ["npm", "start"]
+# Copy the built React app from the builder stage to the Nginx web root
+COPY --from=builder /app/build /usr/share/nginx/html
+
+# Copy the Nginx configuration file to the container
+COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+
+# Expose ports
+EXPOSE 80
+EXPOSE 443
+
+# Start Nginx when the container runs
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube, faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import './Contact.css'; // Create a corresponding CSS file for styling
+import { faEnvelope, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import './Contact.css';
 
 const HomePage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Your server endpoint
+    const serverEndpoint = 'http://localhost:9000/send-email';
+
+    setLoading(true);
+
+    // Send the form data to the server using axios
+    axios.post(serverEndpoint, {
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+    })
+      .then((response) => {
+        console.log('Server response:', response.data);
+        // Optionally, you can reset the form after successful submission
+        setFormData({ name: '', email: '', message: '' });
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   const openMailTo = () => {
-    window.location.href = 'mailto:hasankayan.formal@gmail.com'; // Use 'mailto:' to open the default email client
+    window.location.href = 'mailto:hasankayan.formal@gmail.com';
   };
 
   return (
@@ -29,6 +69,25 @@ const HomePage = () => {
           <FontAwesomeIcon icon={faEnvelope} />
         </button>
       </div>
+
+      {/* Contact Form */}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
+        </div>
+        <button type="submit" className="button submit-button" disabled={loading}>
+          {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : 'Submit'}
+        </button>
+      </form>
     </div>
   );
 };
